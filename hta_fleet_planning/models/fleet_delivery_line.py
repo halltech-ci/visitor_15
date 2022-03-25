abd7ffa5df28aca1879c63d441fc0073c035333a
ATCHE Maurice <maurice.atche@halltech-africa.com>
2022-03-25 05:48:17 +0000

    Adding default value in planning_hours_planned"

--- hta_fleet_planning/models/fleet_delivery_line.py
+++ hta_fleet_planning/models/fleet_delivery_line.py
@@ -7,7 +7,9 @@ class FleetDeliveryLine(models.Model):
     _inherit = 'fleet.delivery.line'
     
     planning_slot_ids = fields.One2many('planning.slot', 'fleet_delivery_line')
-    planning_hours_planned = fields.Float(store=True, compute_sudo=True)
-    planning_hours_to_plan = fields.Float(store=True, compute_sudo=True)
-
+    planning_hours_planned = fields.Float(store=True, default=2.0)
+    planning_hours_to_plan = fields.Float(store=True, default=2.0)
+    state = fields.Selection(related="delivery_id.state")
+    
+    
     
