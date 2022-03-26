d73677149c58d8160919b4c8e5c6f65326155e74
ATCHE Maurice <maurice.atche@halltech-africa.com>
2022-03-26 15:52:19 +0000

    Adding delivery_count in sale order

--- hta_fleet_management/models/sale.py
+++ hta_fleet_management/models/sale.py
@@ -6,7 +6,14 @@ from odoo import models, fields, api
 class SaleOrder(models.Model):
     _inherit="sale.order"
     
+    
+    delivery = fields.Many2many('fleet.delivery', compute='_compute_fleet_delivery')
     delivery_count = fields.Integer(compute="_compute_delivery_line")
+    
+    def _compute_fleet_delivery(self):
+        for rec in self:
+            rec.delivery_ld = self.env['fleet.delivery'].search([('order_id', '=', rec.id)]).id
+            
     
     def _compute_delivery_line(self):
         for rec in self:
